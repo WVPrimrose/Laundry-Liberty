0bf6f7c2a1f71ce76f9205b8aba339cfa8f0ee77
Katie Buchholz <kzbindy@hotmail.com>
2024-06-06 20:40:48 -0400

    started adding the relationships to the index.js file

--- models/index.js
+++ models/index.js
@@ -3,6 +3,15 @@ const Orders = require('.orders.js');
 const Providers = require('.providers.js');
 const Users = require('.user.js');
 
+Users.hasMany(Orders, {
+    foreignKey: 'id',
+    onDelete: 'CASCADE',
+});
+
+Orders.belongsTo(Users, {
+    foreignKey: 'orderNumber'
+});
+
 module.exporets = {
     Orders,
     Providers,
