9ba21732758396742f41bb52dd1abca758374971
Katie Buchholz <kzbindy@hotmail.com>
2024-06-06 20:26:54 -0400

    started working on the index.js in the models folder; now I need to create the relationships within each of the models

---
+++ models/index.js
@@ -0,0 +1,10 @@
+// import models
+const Orders = require('.orders.js');
+const Providers = require('.providers.js');
+const Users = require('.user.js');
+
+module.exporets = {
+    Orders,
+    Providers,
+    Users,
+};
