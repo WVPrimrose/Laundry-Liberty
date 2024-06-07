db05e7e2455e0d7a8abe0bdda679fddeaed4d589
Ameer Clark <ameerclark18@gmail.com>
2024-06-06 21:42:38 -0400

    router updated

---
+++ controlers/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes');
+const orderRoutes = require('./order-routes');
+const providerRoutes = require('./provider-routes');
+
+router.use('/users', userRoutes);
+router.use('/orders', orderRoutes);
+router.use('/providers', providerRoutes);
+
+module.exports = router;
