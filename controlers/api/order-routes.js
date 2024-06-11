54d69dbfcbe7421cc86eac6d4ac38a22a0953456
Ameer Clark <ameerclark18@gmail.com>
2024-06-10 22:02:39 -0400

    controllers updated: order routes and user routes

--- controlers/api/order-routes.js
+++ controlers/api/order-routes.js
@@ -1,2 +1,35 @@
 const router = require('express').Router();
 
+const { Order } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try{
+        const newOrder = await Order.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newOrder);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try{
+        const orderData = await Order.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+        if(!orderData) {
+            res.status(404).json({ message: 'Order number not found'});
+            return;
+        }
+        res.status(200).json(orderData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
